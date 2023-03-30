928db274612a5043c214a5adad4a3fee04775ff3
Terrell-byte <danieltsutton@outlook.com>
2023-03-30 18:09:09 +0200

    fixes

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,5 +3,5 @@ vim.keymap.set('n', '<c-f><c-f>', ':NvimTreeFindFileToggle<CR>')
 vim.keymap.set('n', '<leader>o', ':NvimTreeToggle<CR>')
 
 -- Desc: Keymaps for Nvim
-vim.keymap.set('n', <leader>q, ':q<CR>')
-vim.keymap.set('n', <leader>wq, ':wqa<CR>')
+vim.keymap.set('n', '<leader>q', ':q<CR>')
+vim.keymap.set('n', '<leader>wq', ':wqa<CR>')
