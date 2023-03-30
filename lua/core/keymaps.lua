b13dca80b156459e5bde4d1e1f6ee912758f967a
Terrell-byte <danieltsutton@outlook.com>
2023-03-30 19:30:52 +0200

    fixes

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -11,6 +11,3 @@ vim.keymap.set('n', '<leader>q', ':q<CR>')
 vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>')
 
 -- Desc: Keymaps for Lualine
-
--- Desc: Keymaps for Gitignore
-vim.keymap.set('n', '<leader>gi', gitignore.generate)
