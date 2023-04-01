9452667b1eda921c94419a4c831904d053527eed
Terrell-byte <danieltsutton@outlook.com>
2023-04-01 14:39:43 +0200

    syntax error fix

--- lua/core/plugin_config/which-key.lua
+++ lua/core/plugin_config/which-key.lua
@@ -5,16 +5,16 @@ local mappings = {
   f = {
     name = "+Find",
     f = { "<leader>ff<cr>", "Find Files" },
-  }
+  },
   o = {
     name = "+NvimTree",
     o = { "<leader>o<cr>", "Focus NvimTree" },
     t = { "<leader>ot<cr>", "Toggle NvimTree" },
-  }
+  },
   g = {
     name = "+Git",
     i = { "<leader>gi<cr>", "Gitignore" },
-  }
+  },
   
 }
 local opts = {prefix = '<leader>luk'} -- prefix for all mappings "look up key"
