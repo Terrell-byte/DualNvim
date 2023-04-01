da1741cdb239a54cd92706de70318f42679832fd
Terrell-byte <danieltsutton@outlook.com>
2023-04-01 14:34:28 +0200

    change doc on keymaping

--- lua/core/plugin_config/which-key.lua
+++ lua/core/plugin_config/which-key.lua
@@ -2,10 +2,19 @@ local wk = require("which-key")
 local mappings = {
   q = { "<leader>q<cr>", "Quit" },
   w = { "<leader>wq<cr>", "Save and Quit" },
-  ff = { "<leader>ff<cr>", "Find Files" },
-  ot = { "<leader>ot<cr>", "Toggle NvimTree" },
-  o = { "<leader>o<cr>", "Focus NvimTree" },
-  g = { "<leader>gi<cr>", "Gitignore" },
+  f = {
+    name = "+Find",
+    f = { "<leader>ff<cr>", "Find Files" },
+  }
+  o = {
+    name = "+NvimTree",
+    o = { "<leader>o<cr>", "Focus NvimTree" },
+    t = { "<leader>ot<cr>", "Toggle NvimTree" },
+  }
+  g = {
+    name = "+Git",
+    i = { "<leader>gi<cr>", "Gitignore" },
+  
 }
-local opts = {prefix = '<leader>luk<cr>'}
+local opts = {prefix = '<leader>luk'} -- prefix for all mappings "look up key"
 wk.register(mappings, opts)
