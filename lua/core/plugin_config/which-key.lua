498c5ae744c91dd7bdd60d15680471feabf453b4
Terrell-byte <danieltsutton@outlook.com>
2023-04-01 14:52:54 +0200

    please work right now...

--- lua/core/plugin_config/which-key.lua
+++ lua/core/plugin_config/which-key.lua
@@ -1,21 +1,26 @@
-local wk = require("which-key")
-local mappings = {
-  q = { "<leader>q<cr>", "Quit" },
-  w = { "<leader>wq<cr>", "Save and Quit" },
+wk.register({
   f = {
-    name = "+Find",
-    f = { "<leader>ff<cr>", "Find Files" },
+    name = "file", -- optional group name
+    f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
+    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File", noremap=false, buffer = 123 }, -- additional options for creating the keymap
+    n = { "New File" }, -- just a label. don't create any mapping
+    e = "Edit File", -- same as above
+    ["1"] = "which_key_ignore",  -- special label to hide it in the popup
+    b = { function() print("bar") end, "Foobar" } -- you can also pass functions!
+  },
+  q = { "<cmd>Quit<cr>", "Quit" },
+  w = {
+    name = "Write",
+    w = { "<cmd>Write<cr>", "Write" },
+    q = { "<cmd>WriteQuit<cr>", "Write and Quit" },
   },
   o = {
-    name = "+NvimTree",
-    o = { "<leader>o<cr>", "Focus NvimTree" },
-    t = { "<leader>ot<cr>", "Toggle NvimTree" },
+    name = "NvimTree",
+    o = { "<leader>Focus<cr>", "Focus NvimTree" },
+    t = { "<leader>OpenTree<cr>", "Toggle NvimTree" },
   },
   g = {
-    name = "+Git",
-    i = { "<leader>gi<cr>", "Gitignore" },
+    name = "Git",
+    i = { "<leader>Gitignore<cr>", "Gitignore" },
   },
-  
-}
-local opts = {prefix = '<leader><leader>'} -- prefix for all mappings
-wk.register(mappings, opts)
+}, { prefix = "<leader>" })
