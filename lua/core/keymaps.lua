44a2e9e9cb8545ebbdea787a8380e1f01df6ba2b
Terrell-byte <danieltsutton@outlook.com>
2023-10-31 15:23:31 +0100

    keymap addings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,7 +5,6 @@ vim.keymap.set('n', '<leader>ot', ':NvimTreeToggle<CR>')
 -- Desc: Keymaps for Nvim
 vim.keymap.set('n', '<leader>wq', ':wqa<CR>')
 vim.keymap.set('n', '<leader>q', ':q<CR>')
-
-
+vim.keymap.set('n', '<leader>fn', ':e <C-R>=expand("%:p:h") . "/" <CR>')
 -- Desc: Keymaps for Telescope
 vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>')
